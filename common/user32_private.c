5fc53e334d775b2aedd1e1231dfd0a8ae25a6561
Etienne Dechamps <etienne@edechamps.fr>
2022-02-02 23:07:24 +0000

    Fix user32_private name decoration issues on x86

---
+++ common/user32_private.c
@@ -0,0 +1,4 @@
+// See https://qualapps.blogspot.com/2007/08/how-to-create-32-bit-import-libraries.html for an explanation of the shenanigans going on here.
+
+#define USER32_PRIVATE_STUB
+#include "user32_private.h"
--- common/user32_private.h
+++ common/user32_private.h
@@ -1,8 +1,17 @@
 #include <Windows.h>
 
+#ifdef USER32_PRIVATE_STUB
+#define USER32_PRIVATE_ENTRY_POINT __declspec(dllexport)
+#define USER32_PRIVATE_ENTRY_POINT_DEF {}
+#pragma warning(disable: 4100 4716)
+#else
+#define USER32_PRIVATE_ENTRY_POINT __declspec(dllimport)
+#define USER32_PRIVATE_ENTRY_POINT_DEF
+#endif
+
 // Declarations reverse-engineered from twinui.dll
-__declspec(dllimport) BOOL WINAPI IsShellManagedWindow(HWND hwnd);
-__declspec(dllimport) BOOL WINAPI IsShellFrameWindow(HWND hwnd);
+USER32_PRIVATE_ENTRY_POINT BOOL WINAPI IsShellManagedWindow(HWND hwnd) USER32_PRIVATE_ENTRY_POINT_DEF;
+USER32_PRIVATE_ENTRY_POINT BOOL WINAPI IsShellFrameWindow(HWND hwnd) USER32_PRIVATE_ENTRY_POINT_DEF;
 static const ATOM ATOM_OVERPANNING = 0xa91d;
 // https://blog.adeltax.com/window-z-order-in-windows-10/
-__declspec(dllimport) BOOL WINAPI GetWindowBand(HWND hWnd, PDWORD pdwBand);
+USER32_PRIVATE_ENTRY_POINT BOOL WINAPI GetWindowBand(HWND hWnd, PDWORD pdwBand) USER32_PRIVATE_ENTRY_POINT_DEF;
